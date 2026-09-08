21e9013df352f2338e94a8864d55cc636b186909
Abhishek Gupta <abhigupta639415@gmail.com>
2026-09-09 02:44:29 +0530

    Remove duplicate dns require

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,5 +1,5 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
+   const dns = require('dns');
+   dns.setDefaultResultOrder('ipv4first');
 
 const nodemailer = require('nodemailer');
 
