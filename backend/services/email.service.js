66049b4e35180f054e4672f781a6b6a07f4c3d9c
Abhishek Gupta <abhigupta639415@gmail.com>
2026-09-09 02:38:02 +0530

    email

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -3,31 +3,6 @@ dns.setDefaultResultOrder('ipv4first');
 
 const nodemailer = require('nodemailer');
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     type: 'OAuth2',
-//     user: process.env.EMAIL_USER,
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     refreshToken: process.env.REFRESH_TOKEN,
-//   },
-// });
-
-// // Verify the connection configuration
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.error('Error connecting to email server:', error);
-//   } else {
-//     console.log('Email server is ready to send messages');
-//   }
-// });
-
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
-const nodemailer = require('nodemailer');
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -39,6 +14,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
 // Verify the connection configuration
 transporter.verify((error, success) => {
   if (error) {
